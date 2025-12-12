f20c3e584ff14de3ee897a6d2f6f4af19696fe28
Lorenz Timothy Barco Ranera <lbranera@users.noreply.github.com>
2025-12-13 02:22:38 +0800

    update main.py

--- main.py
+++ main.py
@@ -9,18 +9,16 @@ from tools import get_exponential_params, get_logistic_params
 df = pd.read_csv("data.csv")
 df["t_year"] = df["year"] - df.iloc[0]["year"]
 
-r, P_init = get_exponential_params(df["t_year"], df["population"])
+r_exp, P_init_exp = get_exponential_params(df["t_year"], df["population"])
+K, r_log, P_init_log = get_logistic_params(df["t_year"], df["population"])
 
 years = [i for i in range(1850, 2021)]
 
-predicted_populations = [exponential(year - df.iloc[0]["year"], r, P_init) for year in years]
-plt.plot(years, predicted_populations, color="blue", label="exponential")
+predicted_populations_exp = [exponential(year - df.iloc[0]["year"], r_exp, P_init_exp) for year in years]
+predicted_populations_log = [logistic(year - df.iloc[0]["year"], K, r_log, P_init_log) for year in years]
 
-K, r, P_init = get_logistic_params(df["t_year"], df["population"])
-
-predicted_populations = [logistic(year - df.iloc[0]["year"], K, r, P_init) for year in years]
-plt.plot(years, predicted_populations, color="green", label="logistic")
-
+plt.plot(years, predicted_populations_exp, color="blue", label="exponential")
+plt.plot(years, predicted_populations_log, color="green", label="logistic")
 plt.scatter(df["year"], df["population"], color="red", label="Census Data")
 
 plt.xlabel("Year")
